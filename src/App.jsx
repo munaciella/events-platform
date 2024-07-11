7d05fff9463628ef44b7ff3f0b8ee0e70a39c606
munaciella <franes88@gmail.com>
2024-07-11 20:55:42 +0100

    add navbar components and imported browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import React, { useState } from'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { ThemeProvider } from './components/ThemeProvider';
-import { ModeToggle } from './components/ModeToggle';
+import Navbar from './components/NavBar';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
+    <Router>
+    <div>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-        <ModeToggle />
+        <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/signup" element={<h1>Signup</h1>} />
+        <Route path="/create-event" element={<h1>Create Event</h1>} />
+        </Routes>
       </ThemeProvider>
     </div>
+    </Router>
   );
 }
 
