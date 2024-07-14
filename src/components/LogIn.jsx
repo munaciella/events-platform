ee1af772d8a296ce1831a46e947948ec8fc3fe9c
munaciella <franes88@gmail.com>
2024-07-14 02:34:47 +0100

    first integration with supabase and google auth

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-refresh/only-export-components */
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from './ui/button';
+//import { signInWithGoogle } from '../../supabaseClient';
+
+// const LogIn = () => {
+//   return (
+//     <div className="flex flex-col items-center p-4">
+//       <h1 className="text-3xl font-bold mb-6">Log In</h1>
+//      <Button onClick={signInWithGoogle} className="px-4 py-2">
+//      Log In
+//      </Button>
+//      </div>
+//      );
+//     }
 
 const LogIn = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
