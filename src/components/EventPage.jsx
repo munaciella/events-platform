705d526766e8535317cf7fe56016b7595b30ae8a
munaciella <franes88@gmail.com>
2024-07-16 22:47:56 +0100

    add confirmation page when a user register for event and it adds to table reg

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -122,6 +122,7 @@ import { supabase } from '../../supabaseClient';
 import { format } from 'date-fns';
 import { SkeletonCard } from './ui/SkeletonCard';
 import { Button } from './ui/button';
+import { useSupabaseAuth } from './AuthContext';
 
 const EventPage = () => {
   const { event_id } = useParams();
@@ -129,11 +130,12 @@ const EventPage = () => {
   const [event, setEvent] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { session, userDetails } = useSupabaseAuth();
 
   useEffect(() => {
     const fetchEvent = async () => {
       setLoading(true);
-      let { data: myEvent, error } = await supabase
+      const { data: myEvent, error } = await supabase
         .from('events')
         .select('*')
         .eq('event_id', event_id)
@@ -144,7 +146,7 @@ const EventPage = () => {
       } else {
         setEvent(myEvent);
         setError(null);
-      }
+    }
       setLoading(false);
     };
 
@@ -156,32 +158,37 @@ const EventPage = () => {
   };
 
   const handleRegisterClick = async () => {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession();
+    // const {
+    //   data: { session },
+    // } = await supabase.auth.getSession();
 
     if (!session) {
       navigate('/login');
       return;
     }
 
-    const user = session.user;
+    const user = userDetails;
+    //session.user;
+
+    console.log(`Price: ${event.price}, Pay As You Like: ${event.pay_as_you_like}`);
 
     if (event.price > 0) {
       navigate('/payment');
-    } else if (event.pay_as_you_like) {
-      navigate('/register');
-    } else {
+    } else if (event.price === 0 && event.pay_as_you_like === true) {
       try {
-        const { error } = await supabase
+        const { data, error } = await supabase
           .from('registrations')
-          .insert([{ event_id: event.id, user_id: user.id }]);
+          .insert({ event_id: event.event_id, user_id: user.user_id })
+          .select()
+          .single();
         if (error) {
-          throw error;
+          console.error('Error registering for event:', error.message);
+          setError('Failed to register for event. Please try again later.');
+        } else {
+          navigate(`/confirmation/${event_id}/${data.registration_id}`);
         }
-        navigate('/confirmation', { state: { event } });
       } catch (error) {
-        console.log('Error registering for event:', error);
+        console.error('Unexpected error:', error);
         setError('Failed to register for event. Please try again later.');
       }
     }
@@ -196,13 +203,21 @@ const EventPage = () => {
 
       {!loading && !error && event && (
         <div className="w-full rounded-lg overflow-hidden shadow-lg mt-8">
-          <img src={event.image_url} alt={event.title} className="w-full object-cover h-96" />
+          <img
+            src={event.image_url}
+            alt={event.title}
+            className="w-full object-cover h-96"
+          />
           <div className="flex flex-col items-center p-4">
             <h2 className="text-2xl font-semibold mb-2">{event.title}</h2>
             <p className="text-lg mb-2">{event.description}</p>
             <p className="text-md mb-2">{event.location}</p>
-            <p className="text-sm text-muted-foreground mb-2">{formatDate(event.start_time)}</p>
-            <p className="text-sm text-muted-foreground mb-2">{formatDate(event.end_time)}</p>
+            <p className="text-sm text-muted-foreground mb-2">
+              {formatDate(event.start_time)}
+            </p>
+            <p className="text-sm text-muted-foreground mb-2">
+              {formatDate(event.end_time)}
+            </p>
             <Button
               className="px-4 py-2 dark:bg-foreground-dark"
               onClick={handleRegisterClick}
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react()],
   define: {
     'process.env': {},
