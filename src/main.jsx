6b6da9cd07cd8eae548474f43bd266e1174d295e
munaciella <franes88@gmail.com>
2024-07-17 18:39:28 +0100

    install stripe dependencies, add stripeProvider, wrap stripeProvider in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { SupabaseAuthProvider } from './components/AuthContext.jsx';
+import StripeProvider from './components/StripeProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SupabaseAuthProvider>
+      <StripeProvider>
         <App />
+      </StripeProvider>
     </SupabaseAuthProvider>
   </React.StrictMode>
 );
