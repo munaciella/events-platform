954b7c71e5211242b5c7bedc3d4ed535e427dbf7
munaciella <franes88@gmail.com>
2024-07-21 12:27:25 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,37 @@
+// import { defineConfig, loadEnv } from 'vite';
+// import vercel from 'vite-plugin-vercel';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, __dirname);
+
+//   return {
+//     server: {
+//       port: env.VITE_PORT
+//     },
+//     plugins: [
+//       vercel(),
+//       react(),
+//     ],
+//     build: {
+//       sourcemap: false,
+//     },
+//     resolve: {
+//       alias: {
+//         '@': path.resolve(__dirname, './src'),
+//       },
+//     },
+//     define: {
+//       'process.env': env,
+//     },
+//   };
+// });
+
 import { defineConfig, loadEnv } from 'vite';
 import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
@@ -8,7 +42,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, __dirname);
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     server: {
@@ -27,7 +61,9 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      'process.env': env,
+      'process.env': {
+        VITE_GOOGLE_MAPS_API_KEY: JSON.stringify(env.VITE_GOOGLE_MAPS_API_KEY),
+      },
     },
   };
 });
