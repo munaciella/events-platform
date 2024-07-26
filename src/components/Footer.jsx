6d2b9ef2a294652493f04f395bdb4608e3b2629e
munaciella <franes88@gmail.com>
2024-07-26 11:05:06 +0100

    add border to footer and add background white on dark

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,44 @@
+// import { FaInstagram } from "react-icons/fa";
+// import { FaXTwitter, FaThreads } from "react-icons/fa6";
+
+// export const Footer = () => {
+//   return (
+//     <footer className="w-full flex flex-col md:flex-row justify-between items-center py-3 md:py-6 px-4 mt-20 md:mb-2">
+//       <div className="w-full md:w-auto text-center md:text-left md:flex md:items-center md:justify-start mb-4 md:mb-0">
+//         <h4 className="text-primary md:ml-4">
+//           &copy; {new Date().getFullYear()} EventSphere.
+//         </h4>
+//       </div>
+//       <div className="flex justify-center md:justify-end items-center order-first md:order-last w-full md:w-auto mb-4 md:mb-0 space-x-4 md:space-x-2 md:mr-4">
+//         <a href="https://x.com" target="_blank" rel="noopener noreferrer" className="inline-block text-2xl text-primary hover:text-destructive">
+//           <FaXTwitter />
+//           <span className="sr-only">X</span>
+//         </a>
+//         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="inline-block text-2xl text-primary hover:text-destructive">
+//           <FaInstagram />
+//           <span className="sr-only">Instagram</span>
+//         </a>
+//         <a href="https://www.threads.net/" target="_blank" rel="noopener noreferrer" className="inline-block text-2xl text-primary hover:text-destructive">
+//           <FaThreads />
+//           <span className="sr-only">Threads</span>
+//         </a>
+//       </div>
+//       <div className="w-full text-center md:w-auto md:text-right">
+//         <span className="text-primary">
+//           Made with <span className="text-primary">♡</span> in Mcr
+//         </span>
+//       </div>
+//     </footer>
+//   );
+// };
+
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter, FaThreads } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="w-full flex flex-col md:flex-row justify-between items-center py-3 md:py-6 px-4 mt-20 md:mb-2">
+    <footer className="w-full flex flex-col md:flex-row justify-between items-center py-3 md:py-6 px-4 mt-20 md:mb-2 bg-white shadow-md border-t border-gray-200">
       <div className="w-full md:w-auto text-center md:text-left md:flex md:items-center md:justify-start mb-4 md:mb-0">
         <h4 className="text-primary md:ml-4">
           &copy; {new Date().getFullYear()} EventSphere.
